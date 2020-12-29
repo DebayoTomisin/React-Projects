03d47ab6babaf4dd0343163161f8aff464baaeeb
Debayo Tomisin <debayotomisin07@gmail.com>
2020-12-28 16:28:47 -0800

    completing the main app component

--- 2-fetch-tours/src/App.js
+++ 2-fetch-tours/src/App.js
@@ -1,8 +1,39 @@
+import React, { useState, useEffect } from "react"
+import Loading from "./component/Loading"
+import Tour from './component/Tour'
+
+const url = 'https://course-api.com/react-tours-project'
+
 function App() {
+
+  const [loading, setLoading] = useState(true)
+  const [tours, setTours] = useState([])
+
+  useEffect(() => {
+    const fetchTours = async () => {
+      
+      setLoading(true)
+
+      try {
+        const response = await fetch(url)
+        const tours = await response.json()
+        setTours(tours)
+        console.log(tours)
+      } 
+      catch (error) {
+        console.log(error)
+      }
+    }
+
+    setLoading(false)
+    fetchTours()
+
+  }, [])
+
   return (
-    <div className="App">
-      <h2>set up time</h2>
-    </div>
+    <main>
+      {loading ? <Loading/> : <Tour tours={tours}/>}
+    </main>
   );
 }
 
