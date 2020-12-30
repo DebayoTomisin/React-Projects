2ead524870b2f41ead19a7b950db25392294240b
Debayo Tomisin <debayotomisin07@gmail.com>
2020-12-29 23:21:49 -0800

    final code

--- 3-reviews/src/App.js
+++ 3-reviews/src/App.js
@@ -1,10 +1,16 @@
-import './App.css';
+import React, { useSate } from 'react'
+import Reviews from './review'
 
 function App() {
   return (
-    <div className="App">
-      <h2>This is getting heavy can you hear the bass boom! I'm ready!</h2>
-    </div>
+    <main>
+      <section className="container">
+        <h2>Our Reviews</h2>
+        <div className="underline"></div>
+        <Reviews />
+      </section>
+    </main>
+    
   );
 }
 
