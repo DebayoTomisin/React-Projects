498b4058840620b15fe4a05de094c3bc03719b9a
Debayo Tomisin <debayotomisin07@gmail.com>
2021-01-02 20:29:26 -0800

    app and index

--- 5-menu/src/App.js
+++ 5-menu/src/App.js
@@ -1,9 +1,34 @@
+import { useState } from 'react'
+import Menu from './Menu'
+import Category from './Categories'
+import data from './data'
+
+const allCategories = ['all', ...new Set(data.map(item => item.category))]
 
 function App() {
+
+  const [menuItems, setMenuItem] = useState(data)
+  const [categories, setCategories] = useState(allCategories)
+  
+  const filterCats = (category) => {
+    if(category = 'all'){
+      setMenuItem(data)
+      return 
+    }
+    const newCat = data.filter(item => item.category === category)
+    setMenuItem(newCat)
+  }
+  
   return (
-    <div>
-      <h2> Set Up  </h2>      
-    </div>
+    <main>
+      <section className="menu section">
+        <div className="title"><h2>Our Menu</h2></div>
+        <div className="underline"></div>
+
+        <Category categories={categories} filterCats= {filterCats}/>
+        <Menu menuItems={menuItems}/>
+      </section>
+    </main>
   );
 }
 
