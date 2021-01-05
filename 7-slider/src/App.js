170084515ee7b00226916a8d36fc187d45e18770
Debayo Tomisin <debayotomisin07@gmail.com>
2021-01-05 13:05:53 -0800

    app & index css commit

--- 7-slider/src/App.js
+++ 7-slider/src/App.js
@@ -1,8 +1,54 @@
+import React, { useState, useEffect } from "react"
+import data from './component/data'
+import Button from './component/Button'
+import People from './component/People'
+
 function App() {
+
+  const [ people, setPeople ] = useState(data)
+  const [ index, setIndex ] =  useState(0)
+
+  const nextSlide = () => {
+    setIndex((oldIndex) =>{
+      let index = oldIndex + 1
+      if(index > people.length - 1){
+        index = 0
+      }
+      return index
+    })
+  }
+
+  const prevSlide = () => {
+    setIndex((oldIndex) => {
+      let index = oldIndex - 1
+      if(index < 0){
+        index = people.length - 1
+      }
+      return index
+    })
+  }
+
+  useEffect(() => {
+    let slider = setInterval(() => {
+      nextSlide()
+    }, 5000)
+    return() => {
+      clearInterval(slider)
+    }
+  }, [ index ])
+
   return (
-    <div className="App">
-      <h2>Something different</h2>
-    </div>
+    <section className="section">
+      <div className="title">
+        <h2><span>/</span>Reviews</h2>
+      </div>
+
+      <div className="section-center">
+      <People people={people} index = {index}/>
+      <Button prevSlide={prevSlide} nextSlide={nextSlide}/>
+      </div>
+      
+    </section>
   );
 }
 
