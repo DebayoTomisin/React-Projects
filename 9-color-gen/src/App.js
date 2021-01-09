9e5ccac59aae74cd7bd4973cf6704de485b4e1d0
Debayo Tomisin <debayotomisin07@gmail.com>
2021-01-09 01:13:44 -0800

    packages installed

--- 9-color-gen/src/App.js
+++ 9-color-gen/src/App.js
@@ -1,8 +1,33 @@
-import './App.css';
+import React, { useState } from 'react'
+
+import Values from 'values.js'
+import SingleColor from './SingleColor'
+
 
 function App() {
+  
+  const [color, setColor] = useState('')
+  const [error, setError] = useState(false)
+  const [list, setList] = useState([])
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    
+    try {
+      let colors = new Values(color).all(10)
+      setColor(colors)
+      
+    } catch (error) {
+      setError(true)
+      console.log(error)
+    }
+  }
+
   return (
-    <h2>I want it that way!</h2>
+    <>
+      <section className="container"></section>
+      <section className="colors"></section>
+    </>
   );
 }
 
