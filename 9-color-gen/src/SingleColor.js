e7b5a95ec576b9919d27182f8e412186126774fd
Debayo Tomisin <debayotomisin07@gmail.com>
2021-01-09 01:16:02 -0800

    src folder commit

---
+++ 9-color-gen/src/SingleColor.js
@@ -0,0 +1,8 @@
+import React, { useState, useEffect } from 'react'
+
+const SingleColor = ({index, hexColor, }) => {
+    return(
+        <h2>Single color component</h2>
+    )
+}
+export default SingleColor
