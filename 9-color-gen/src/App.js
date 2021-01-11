fe59d0cdd987949645aaad165d0b670a648a3f91
Debayo Tomisin <debayotomisin07@gmail.com>
2021-01-11 13:11:13 -0800

    bug with validation class

--- 9-color-gen/src/App.js
+++ 9-color-gen/src/App.js
@@ -8,25 +8,43 @@ function App() {
   
   const [color, setColor] = useState('')
   const [error, setError] = useState(false)
-  const [list, setList] = useState([])
+  const [list, setList] = useState(new Values('#f0f0f0').all(10))
 
   const handleSubmit = (e) => {
     e.preventDefault()
-    
-    try {
+    try{
       let colors = new Values(color).all(10)
-      setColor(colors)
-      
-    } catch (error) {
+      setList(colors) 
+    } catch(error){
       setError(true)
       console.log(error)
     }
   }
 
+
   return (
     <>
-      <section className="container"></section>
-      <section className="colors"></section>
+      <section className="container">
+        
+        <h3>color generator</h3>
+
+        <form onSubmit={handleSubmit}>
+        <input
+            type='text'
+            value={color}
+            onChange={(e) => setColor(e.target.value)}
+            placeholder='#f15025'
+            className={`${error ? 'error' : null}`}
+          />
+          <button className="btn" type="submit">generate</button>
+        </form>
+      </section
+      >
+      <section className="colors">
+        {list.map((color, index) => {
+          return <SingleColor key={index} index={index} {...color} hexColor={color.hex}/>
+        })}
+      </section>
     </>
   );
 }
