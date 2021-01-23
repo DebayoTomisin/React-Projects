d516daf8c8c144d2ab62856a8e5d46ff47ef6b7e
Debayo Tomisin <debayotomisin07@gmail.com>
2021-01-22 21:27:46 -0800

    gitingnore

--- 12-sidebar-modal/src/App.js
+++ 12-sidebar-modal/src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
+import logo from './logo192.png';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <img src={logo} alt="logo"/>
+      <h1>This is getting heavy can you hear the bass boom i'm ready</h1>
+    </>
   );
 }
 
