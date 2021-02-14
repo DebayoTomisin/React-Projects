0cb26360b2545be05711a71d12bfe30fc9567058
Debayo Tomisin <debayotomisin07@gmail.com>
2021-02-14 10:29:00 -0800

    dispatch has issues with state when i try tp dispatch

--- 14-cart/src/context.js
+++ 14-cart/src/context.js
@@ -31,6 +31,10 @@ const AppProvider = ({ children }) => {
     dispatch({ type: "DECREASE", payload: id })
   }
 
+  useEffect(() => {
+    console.log(state.cart)
+  }, [state.cart])
+
   
   return(
     <AppContext.Provider value= {{
