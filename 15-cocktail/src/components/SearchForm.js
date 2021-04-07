f55f8f98fa4da7424e68f440021fd0063006476f
Debayo Tomisin <debayotomisin07@gmail.com>
2021-04-07 01:45:03 -0700

    search form and single cocktail page done, bug in ingridients

--- 15-cocktail/src/components/SearchForm.js
+++ 15-cocktail/src/components/SearchForm.js
@@ -1,8 +1,37 @@
 import React, {useState, useRef, useEffect } from "react"
+import { useGlobalContext } from "../context"
 
 const SearchForm = () => {
+
+    const {setSearchTerm} = useGlobalContext()
+    const searchValue = useRef('')
+
+    const handleChange = () => {
+        setSearchTerm(searchValue.current.value)
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+    }
+
+    useEffect(() => {
+        searchValue.current.focus()
+    })
+
     return(
-        <h1>Search Form component</h1>
+        <section className="section search">
+            <form className="search-form" onSubmit={handleSubmit}>
+                <div className="form-control">
+                    <label htmlFor="name">Search your favourite cocktail</label>
+                    <input 
+                        id="name" 
+                        type="text"
+                        ref={searchValue}
+                        onChange={handleChange}
+                    />
+                </div>
+            </form>
+        </section>
     )
 }
 
